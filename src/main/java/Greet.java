1701c642f332d2d95c1c93f06847e545fb5aa130
Isaaclks7 <e1122338@u.nus.edu>
2025-01-29 00:48:28 +0800

    Add Echo in Greet.java

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Greet {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Ducky");
-        System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Ducky.");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        String line;
+
+        while (true) {
+            line = in.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + line);
+            System.out.println("    ____________________________________________________________");
+        }
+
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
